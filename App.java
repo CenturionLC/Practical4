bae23c72ffaf67017f527bd77d5c06c3b5fb2b03
CenturionLC <100900058+CenturionLC@users.noreply.github.com>
2022-05-03 11:38:16 +0200

    Add files via upload
    Fixed enviroment variables for DB, now uses system var's, not hardcoded.

--- App.java
+++ App.java
@@ -2,13 +2,13 @@ package za.ac.up.cs.cos221;
 
 import javax.swing.*;
 import javax.swing.table.*;
-import javax.swing.event.*;
 import java.sql.*;
 import java.awt.*;
 
 public class App {
-    JFrame fra;
-    JTable staffTable; 
+    JFrame fra, f;
+    JTable staffTable;
+
     App() throws SQLException {
         fra = new JFrame();
         JPanel p1 = new JPanel();
@@ -32,36 +32,55 @@ public class App {
         p1.repaint();
         JScrollPane info2 = new JScrollPane(filmPanel());
         p2.add(info2);
-        JTextField filterFilm = new JTextField();
-        filterFilm.setPreferredSize(new Dimension(100, 30));
-        JButton Filmb = new JButton("Filter");
-        p1.add(filterFilm);
-        p1.add(Filmb);
+        JTextField filterStaff = new JTextField();
+        filterStaff.setPreferredSize(new Dimension(100, 30));
+        JButton Staffb = new JButton("Filter");
+        p1.add(filterStaff);
+        p1.add(Staffb);
         p1.revalidate();
         p1.repaint();
+        JButton Filmb = new JButton("Add Data");
+        p2.add(Filmb);
 
         TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(staffTable.getModel());
         staffTable.setRowSorter(sorter);
 
-        Filmb.addActionListener(a -> {
-            String text = filterFilm.getText();
-            if(text == ""){
+        Staffb.addActionListener(a -> {
+            String text = filterStaff.getText();
+            if (text == "") {
                 sorter.setRowFilter(null);
-            }
-            else{
+            } else {
                 sorter.setRowFilter(RowFilter.regexFilter("(?i)" + text));
-                
+
+            }
+        });
+
+        Filmb.addActionListener(a -> {
+            try {
+                addFilm();
+                p2.revalidate();
+                p2.repaint();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         });
         
-}
-    
-    private static String driver = "jdbc:mariadb";
+
+    }
+
+    /*private static String driver = "jdbc:mariadb";
     private static String host = "localhost";
     private static int port = 3307;
     private static String database = "u21489549_sakila";
     private static String username = "root";
-    private static String password = "Blackcat";
+    private static String password = "Blackcat";*/
+
+    private static String driver = System.getenv("SAKILA_DB_PROTO");
+    private static String host = System.getenv("SAKILA_DB_HOST");
+    private static String port = System.getenv("SAKILA_DB_PORT");
+    private static String database = System.getenv("SAKILA_DB_NAME");
+    private static String username = System.getenv("SAKILA_DB_USERNAME");
+    private static String password = System.getenv("SAKILA_DB_PASSWORD");
 
     public JTable staffPanel() throws SQLException {
         int rows = 0;
@@ -73,15 +92,16 @@ public class App {
         Connection connection = DriverManager.getConnection(url, username, password);
         Statement statement = connection.createStatement();
 
-        /*try (ResultSet result = statement.executeQuery("SELECT 1")) {
-            System.out.println("Connection to database " + database + " on host "
-                    + host + ":" + port + " successfully established");
-        }*/
+        /*
+         * try (ResultSet result = statement.executeQuery("SELECT 1")) {
+         * System.out.println("Connection to database " + database + " on host "
+         * + host + ":" + port + " successfully established");
+         * }
+         */
 
-        
         ResultSet result = statement.executeQuery(
                 "SELECT * FROM staff");
-        while(result.next()){
+        while (result.next()) {
             rows++;
         }
         ResultSet result2 = statement.executeQuery(
@@ -123,10 +143,9 @@ public class App {
                     + host + ":" + port + " successfully established");
         }
 
-        
         ResultSet result = statement.executeQuery(
                 "SELECT * FROM film");
-        while(result.next()){
+        while (result.next()) {
             rows++;
         }
         ResultSet result2 = statement.executeQuery(
@@ -144,16 +163,61 @@ public class App {
             film[i][7] = result2.getString("category_id");
             i++;
         }
-        String coloumNames[] = {"Title", "Released", "Language", "Rent time", "Rent cost", "Lost cost", "rating", "category"};
-
+        String coloumNames[] = { "Title", "Released", "Language", "Rent time", "Rent cost", "Lost cost", "rating",
+                "category" };
 
         JTable filmTable = new JTable(film, coloumNames);
         return filmTable;
     }
 
-    
-    
+    public void addFilm() throws SQLException {
+            String url = new StringBuilder()
+                    .append(driver).append("://")
+                    .append(host).append(":").append(port).append("/")
+                    .append(database)
+                    .toString();
+            Connection connection = DriverManager.getConnection(url, username, password);
+        
+            /*try (ResultSet result = statement.executeQuery("SELECT 1")) {
+                System.out.println("Connection to database " + database + " on host "
+                        + host + ":" + port + " successfully established");
+            }*/
 
+            f =new JFrame(); 
+            JOptionPane.showMessageDialog(f, "Enter each bit of data one at a time, press ok after each entry!");
+            String title = JOptionPane.showInputDialog(f, "Enter Title");
+            String Des = JOptionPane.showInputDialog(f, "Enter Description");
+            int rYear = Integer.parseInt(JOptionPane.showInputDialog(f, "Enter Release year"));
+            int lanId = Integer.parseInt(JOptionPane.showInputDialog(f, "Enter Language id"));
+            int orgLanId = Integer.parseInt(JOptionPane.showInputDialog(f, "Enter Original Language ID"));
+            int rDur = Integer.parseInt(JOptionPane.showInputDialog(f, "Enter Rental Duration"));
+            double rRate = Double.parseDouble(JOptionPane.showInputDialog(f, "Enter Rental Rate"));
+            int len = Integer.parseInt(JOptionPane.showInputDialog(f, "Enter Length"));
+            double rCost = Double.parseDouble(JOptionPane.showInputDialog(f, "Enter Replacement Cost"));
+            String rating = JOptionPane.showInputDialog(f, "Enter Rating");
+            String speF = JOptionPane.showInputDialog(f, "Enter Special features");
+
+            try(PreparedStatement filmPrepared = connection.prepareStatement("INSERT INTO film (title, description, release_year, language_id, original_language_id, rental_duration, rental_rate, length, replacement_cost, rating, special_features) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")){
+                filmPrepared.setString(1, title);
+                filmPrepared.setString(2, Des);
+                filmPrepared.setInt(3, rYear);
+                filmPrepared.setInt(4, lanId);
+                if(orgLanId == 0){
+                    filmPrepared.setNull(5, orgLanId);
+                }
+                else{
+                    filmPrepared.setInt(5, orgLanId);
+                }
+                filmPrepared.setInt(6, rDur);
+                filmPrepared.setDouble(7, rRate);
+                filmPrepared.setDouble(8, len);
+                filmPrepared.setDouble(9, rCost);
+                filmPrepared.setString(10, rating);
+                filmPrepared.setString(11, speF);
+
+                filmPrepared.execute();
+            }
+    }
 
     public static void main(String[] args) throws SQLException {
 
